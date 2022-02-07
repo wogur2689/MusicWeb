997e83e9dafaf02059e0f2e0990268feb9977e37
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-02-07 12:49:36 +0900

    22.02.07

--- src/component/main/Var/Var.js
+++ src/component/main/Var/Var.js
@@ -1,9 +1,13 @@
 import styles from './Var.module.css';
+import { IoMdPlayCircle } from "react-icons/io";
+import { IoStopCircle } from "react-icons/io5";
 
 function Var() {
     return (
         <div className={styles.music_var}> 
             <div className={styles.time1}>3:32</div>
+                <IoMdPlayCircle className={styles.icon}/>
+                <IoStopCircle className={styles.icon}/>
                 <div className={styles.max_time}>
                     <div className={styles.date_time}></div>
                 </div>
--- src/component/main/list/List.js
+++ src/component/main/list/List.js
@@ -1,31 +1,28 @@
 import styles from './List.module.css';
+//import sunset from '../../img/sunset.jpg';
 
 function List() {
     return (
         <div className={styles.music_list}>
             <div className={styles.music_list_item}>
-                <div className={styles.music_list_img}>이미지</div>
-                <div className={styles.music_list_title}>음악 1</div> 
-                <div className={styles.music_list_singer}>가수</div>
-                <div className={styles.music_list_date}>발매년도</div>
+                <div className={styles.music_list_img}></div>
+                <div className={styles.music_list_title}>노을</div> 
+                <div className={styles.music_list_singer}>사운드포엠</div>
             </div>
             <div className={styles.music_list_item}>
                 <div className={styles.music_list_img}>이미지</div>
                 <div className={styles.music_list_title}>음악 2</div>
                 <div className={styles.music_list_singer}>가수</div>
-                <div className={styles.music_list_date}>발매년도</div>
             </div>
             <div className={styles.music_list_item}>
                 <div className={styles.music_list_img}>이미지</div>
                 <div className={styles.music_list_title}>음악 3</div>
                 <div className={styles.music_list_singer}>가수</div>
-                <div className={styles.music_list_date}>발매년도</div>
             </div>
             <div className={styles.music_list_item}>
                 <div className={styles.music_list_img}>이미지</div>
                 <div className={styles.music_list_title}>음악 4</div>
                 <div className={styles.music_list_singer}>가수</div>
-                <div className={styles.music_list_date}>발매년도</div>
             </div>
         </div>
     );
