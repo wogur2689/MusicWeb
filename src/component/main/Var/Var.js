466d7695d91011c7aef0822a27dade622e085609
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-03-03 00:54:19 +0900

    react-jinke-music-player npm_modules 추가

--- src/component/main/Var/Var.js
+++ src/component/main/Var/Var.js
@@ -2,28 +2,15 @@ import styles from './Var.module.css';
 import { IoMdPlayCircle } from "react-icons/io";
 import { IoStopCircle } from "react-icons/io5";
 import { useState } from 'react';
-import {Howl, Howler} from 'howler';
 
-/*
-document.querySelector(".btn1").addEventListener("click", function () {
-    var audio1 = new Audio("sound1.mp3");
-    audio1.loop = false; // 반복재생하지 않음
-    audio1.volume = 0.5; // 음량 설정
-    audio1.play(); // sound1.mp3 재생
-  });
-*/
 function Var() {
 
     const [play, setPlay] = useState(false);
-    const sound = new Howl({
-        src: ['../../data/노을.mp3'],
-        volume: 5.0
-    });
 
     function playing () {
         setPlay(play => !play)
         if(play === true) {
-            sound.play();
+            //sound.play();
         }
     }
 
@@ -41,5 +28,3 @@ function Var() {
         </div>
     );
 }
-
-export default Var;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  <ReactJkMusicPlayer {...options} />,
   document.getElementById('root')
 );
 
