a0b5cfc0b09b02f796b4af044002054b57a159bf
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-05-07 16:03:32 +0900

    라디오 버튼 구현

--- src/component/main/list/List.js
+++ src/component/main/list/List.js
@@ -1,27 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import styles from './List.module.css';
 import sunset from '../../img/sunset.jpg';
 
 function List() {
-    const [selection1, setSelection1] = useState(false); //처음 노래만 체크
-    const [selection2, setSelection2] = useState(true);
-    const [selection3, setSelection3] = useState(true);
-    const [selection4, setSelection4] = useState(true);
+    const [selection, setSelection] = useState(true);
 
     const musicSelection = (e) => {
-        //e.preventDefalut();
         let current = document.getElementById(e.target.id);
-        if(current.id === "case1") {
-            if(current.className === styles.music_selection) {
-                setSelection1(!selection1);
-            }
-        } else if(current.id === "case2") {
-            if(current.className === styles.music_selection) setSelection2(!selection2);
-        } else if(current.id === "case3") {
-            if(current.className === styles.music_selection) setSelection3(!selection3);
-        } else if(current.id === "case4") {
-            if(current.className === styles.music_selection) setSelection4(!selection4);
-        }
+        console.log(current);
+        setSelection(!selection);
     }
 
     return (
@@ -32,25 +19,25 @@ function List() {
                 </div>
                 <div className={styles.music_list_title}>노을</div> 
                 <div className={styles.music_list_singer}>사운드포엠</div>
-                <button id="case1" className={selection1 === true ? styles.music_selection : styles.music_not_selected} onClick={musicSelection}>선택</button>
+                <label><input type="radio" id="case1" className={selection === true ? styles.music_selection : styles.music_not_selected} onClick={musicSelection} name="music"/>선택</label>
             </div>
             <div className={styles.music_list_item}>
                 <div className={styles.music_list_img}>이미지</div>
                 <div className={styles.music_list_title}>음악 2</div>
                 <div className={styles.music_list_singer}>가수</div>
-                <button id="case2" className={selection2 === true ? styles.music_selection : styles.music_not_selected} onClick={musicSelection}>선택</button>
+                <label><input type="radio" id="case2" className={selection === true ? styles.music_selection : styles.music_not_selected} onClick={musicSelection} name="music"/>선택</label>
             </div>
             <div className={styles.music_list_item}>
                 <div className={styles.music_list_img}>이미지</div>
                 <div className={styles.music_list_title}>음악 3</div>
                 <div className={styles.music_list_singer}>가수</div>
-                <button id="case3" className={selection3 === true ? styles.music_selection : styles.music_not_selected} onClick={musicSelection}>선택</button>
+                <label><input type="radio" id="case3" className={selection === true ? styles.music_selection : styles.music_not_selected} onClick={musicSelection} name="music"/>선택</label>
             </div>
             <div className={styles.music_list_item}>
                 <div className={styles.music_list_img}>이미지</div>
                 <div className={styles.music_list_title}>음악 4</div>
                 <div className={styles.music_list_singer}>가수</div>
-                <button id="case4" className={selection4 === true ? styles.music_selection : styles.music_not_selected} onClick={musicSelection}>선택</button>
+                <label><input type="radio" id="case4" className={selection === true ? styles.music_selection : styles.music_not_selected} onClick={musicSelection} name="music"/>선택</label>
             </div>
         </div>
     );
