4ba8c68c751b0a255422aa2f75cbc6387f8d9d4c
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-05-15 21:45:55 +0900

    초기값 설정 + 선택한 버튼 id값 수신

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -1,21 +1,38 @@
+import { useState } from 'react';
 import WaveWrapper from './waveWrapper/WaveWrapper';
 import List from './list/List';
-import Information from './information/Information';
+import Information from './Information/Information';
 import Var from './var/Var';
 import styles from './Main.module.css';
 import music from './data/musicData.json';
 
 function Main() {
+    const [targetMusic, setTargetMusic] = useState(
+        {"id": 0, 
+        "title" : "선택되지 않음", 
+        "singer": "선택되지 않음",
+        "src": "music/sunset.mp3",
+        "img": "img/sunset.jpg"}
+    );
+
+    const dataFunction = (e) => {
+        setTargetMusic(music.music.find(element => 
+            element.id === e
+        ));
+    };
+
     return (
-            <div className={styles.section}>
-                <WaveWrapper /> 
-                <div className={styles.main}>
-                    <List className={styles.List} />
-                    <div className={styles.music_img}></div>
-                    <Information className={styles.Information}/>
-                    <Var className={styles.Var}/>
+        <div className={styles.section}>
+            <WaveWrapper /> 
+            <div className={styles.main}>
+                <List dataFunction={dataFunction} className={styles.List} />
+                <div className={styles.music_img}>
+                    <img src={`${process.env.PUBLIC_URL}/data/${targetMusic.img}`} width="100%" height="100%" alt="사진" />
                 </div>
+                <Information className={styles.Information}/>
+                <Var className={styles.Var}/>
             </div>
+        </div>
     );
 }
 
